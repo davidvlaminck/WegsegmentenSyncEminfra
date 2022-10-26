00fda795dbd6b36c2aa0bcff455084bfb555d547
davidvlaminck <davidvlaminck85@gmail.com>
2022-10-26 17:06:37 +0200

    update for lantis

--- uitvoering.py
+++ uitvoering.py
@@ -49,6 +49,7 @@ def district_ref_to_name_mapping(ref):
 def add_exceptions(segmenten):
     instances_to_remove = []
     for instance in segmenten:
+        # brabo 1
         if instance.ident8 == 'N0100001':
             if 'AWV121' in instance.gebied and instance.begin.opschrift == 0:
                 instances_to_remove.append(instance)
@@ -69,8 +70,25 @@ def add_exceptions(segmenten):
                 instance.begin.opschrift = 1.2
                 instance.begin.afstand = 0
                 instance.lengte = instance.eind.positie - instance.begin.positie
-
-    # N1120001;0;-10;3.9;53;Agentschap Wegen en Verkeer - AWV123
+        # LANTIS
+        if instance.ident8 == 'A0140001' and 'AWV121' in instance.gebied:
+            instance.gebied = 'LANTIS'
+        if instance.ident8 == 'R0010001' and 'AWV121' in instance.gebied:
+            instance.eind.positie = 14.350
+            instance.eind.opschrift = 14.350
+            instance.eind.afstand = 0
+            instance.lengte = instance.eind.positie - instance.begin.positie
+        if instance.ident8 == 'A0110002' and 'AWV121' in instance.gebied:
+            instance.begin.positie = 68.0
+            instance.begin.opschrift = 68.0
+            instance.begin.afstand = 0
+            instance.lengte = instance.eind.positie - instance.begin.positie
+        # N0700001;37.9;-21;49.8;1;Agentschap Wegen en Verkeer - AWV121
+        if instance.ident8 == 'N0700001' and 'AWV121' in instance.gebied:
+            instance.eind.positie = 48.5
+            instance.eind.opschrift = 48.5
+            instance.eind.afstand = 0
+            instance.lengte = instance.eind.positie - instance.begin.positie
 
     for instance_to_remove in instances_to_remove:
         segmenten.remove(instance_to_remove)
@@ -116,6 +134,62 @@ def add_exceptions(segmenten):
     n112_brabo.eind.opschrift = 1.2
     n112_brabo.eind.afstand = 0
     segmenten.append(n112_brabo)
+
+    r1_lantis = EventDataSegment()
+    r1_lantis.begin = WegLocatieData()
+    r1_lantis.eind = WegLocatieData()
+    r1_lantis.ident8 = 'R0010001'
+    r1_lantis.gebied = 'LANTIS'
+    r1_lantis.eigenbeheer = False
+    r1_lantis.begin.positie = 14.350
+    r1_lantis.begin.opschrift = 14.350
+    r1_lantis.begin.afstand = 0
+    r1_lantis.eind.positie = 16.706
+    r1_lantis.eind.opschrift = 16.706
+    r1_lantis.eind.afstand = 0
+    segmenten.append(r1_lantis)
+
+    a11_lantis = EventDataSegment()
+    a11_lantis.begin = WegLocatieData()
+    a11_lantis.eind = WegLocatieData()
+    a11_lantis.ident8 = 'A0110002'
+    a11_lantis.gebied = 'LANTIS'
+    a11_lantis.eigenbeheer = False
+    a11_lantis.begin.positie = 63.768
+    a11_lantis.begin.opschrift = 64.1
+    a11_lantis.begin.afstand = -332
+    a11_lantis.eind.positie = 68.0
+    a11_lantis.eind.opschrift = 68.0
+    a11_lantis.eind.afstand = 0
+    segmenten.append(a11_lantis)
+
+    n70_lantis = EventDataSegment()
+    n70_lantis.begin = WegLocatieData()
+    n70_lantis.eind = WegLocatieData()
+    n70_lantis.ident8 = 'N0700001'
+    n70_lantis.gebied = 'LANTIS'
+    n70_lantis.eigenbeheer = False
+    n70_lantis.begin.positie = 48.5
+    n70_lantis.begin.opschrift = 48.5
+    n70_lantis.begin.afstand = 0
+    n70_lantis.eind.positie = 49.12
+    n70_lantis.eind.opschrift = 49.1
+    n70_lantis.eind.afstand = 20
+    segmenten.append(n70_lantis)
+
+    n70_awv = EventDataSegment()
+    n70_awv.begin = WegLocatieData()
+    n70_awv.eind = WegLocatieData()
+    n70_awv.ident8 = 'A0110002'
+    n70_awv.gebied = 'Agentschap Wegen en Verkeer - AWV121'
+    n70_awv.eigenbeheer = False
+    n70_awv.begin.positie = 49.12
+    n70_awv.begin.opschrift = 49.1
+    n70_awv.begin.afstand = 20
+    n70_awv.eind.positie = 49.801
+    n70_awv.eind.opschrift = 49.8
+    n70_awv.eind.afstand = 1
+    segmenten.append(n70_awv)
 
     return segmenten
 
@@ -362,23 +436,23 @@ if __name__ == '__main__':
             lines_csv.append(f'{segment_WDB.id};{best_match.naampad};orig;{simplified}')
             lines_csv.append(f'{segment_WDB.id};{best_match.naampad};simplified;{simplified}')
 
-            if segment_WDB.wktLineStringZ != best_match.geometrie:
-                response = requester.put(
-                    url=f'eminfra/core/api/installaties/{best_match.uuid}/installaties/ops/update-locatie-ident8',
-                    json={
-                        "uuids": [best_match.uuid],
-                        "async": False,
-                        "ident8": segment_WDB.ident8
-                    })
-                print(response.status_code)
-
-                response = requester.put(url=f'eminfra/core/api/installaties/{best_match.uuid}'
-                                             '/kenmerken/80052ed4-2f91-400c-8cba-57624653db11/geometrie',
-                                         json={"geometrie": str(simplified)})
-                print(response.status_code)
-
-
-            pass
+            # if segment_WDB.wktLineStringZ != best_match.geometrie:
+            #     response = requester.put(
+            #         url=f'eminfra/core/api/installaties/{best_match.uuid}/installaties/ops/update-locatie-ident8',
+            #         json={
+            #             "uuids": [best_match.uuid],
+            #             "async": False,
+            #             "ident8": segment_WDB.ident8
+            #         })
+            #     print(response.status_code)
+            #
+            #     response = requester.put(url=f'eminfra/core/api/installaties/{best_match.uuid}'
+            #                                  '/kenmerken/80052ed4-2f91-400c-8cba-57624653db11/geometrie',
+            #                              json={"geometrie": str(simplified)})
+            #     print(response.status_code)
+            #
+            #
+            # pass
 
     for matched_segment in matched_segments_WDB:
         segmenten_WDB.remove(matched_segment)
@@ -389,8 +463,8 @@ if __name__ == '__main__':
         else:
             eminfra_data.remove(matched_eminfra)
 
-    print(f'count eminfra data: {len(eminfra_data)}')
-    print(f'count WDB data: {len(segmenten_WDB)}')
+    print(f'count remaining eminfra data: {len(eminfra_data)}')
+    print(f'count remaining WDB data: {len(segmenten_WDB)}')
 
     if len(df) > 0:
         df.to_excel('beheersegmenten_correcties.xlsx')
